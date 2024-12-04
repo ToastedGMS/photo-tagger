45d4684af763317b58d4594a78334d1847fe85c7
ToastedGMS <gabrielsiqueira3052@gmail.com>
2024-12-04 13:52:16 -0300

    display image on component render

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import image1 from './public/image1.jpeg';
 
 function App() {
 	return (
 		<>
 			<h1>Hello world</h1>
+			<img src={image1} alt="Example Image" />
 		</>
 	);
 }
