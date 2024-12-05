79fa6123c014a53656b8709e749f1ded3fa7fd54
ToastedGMS <gabrielsiqueira3052@gmail.com>
2024-12-05 19:38:56 -0300

    add a custom cursor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import image1 from './public/image1.jpeg';
+import styles from './stylesheets/App.module.css';
 
 function App() {
 	return (
 		<>
 			<h1>Hello world</h1>
-			<img src={image1} alt="Example Image" />
+			<img src={image1} alt="Example Image" className={styles.image1} />
 		</>
 	);
 }
