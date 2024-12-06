d4cedefc5f32d0817e3c90dec71cea04dcc1acb8
ToastedGMS <gabrielsiqueira3052@gmail.com>
2024-12-06 14:32:55 -0300

    add logic for responsively mapping target on image

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import image1 from './public/image1.jpeg';
 import styles from './stylesheets/App.module.css';
+import imageMapResize from 'image-map-resizer';
 
 function App() {
+	useEffect(() => {
+		imageMapResize();
+	}, []);
+
 	return (
 		<>
 			<div className={styles.container}>
-				<img src={image1} alt="Example Image" className={styles.image1} />
+				<img
+					src={image1}
+					alt="Example Image"
+					className={styles.image1}
+					useMap="#charmander"
+				/>
+				<map name="charmander">
+					<area
+						shape="rect"
+						coords="481,615,545,680"
+						title="Charmander"
+						className={styles.pokemon}
+						onClick={() => {
+							console.log('Charmander');
+						}}
+					/>
+				</map>
 			</div>
 		</>
 	);
