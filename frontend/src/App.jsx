545fc535f4680c5f09d37dc304d68c86eac10970
ToastedGMS <gabrielsiqueira3052@gmail.com>
2024-12-08 14:02:52 -0300

    add game start modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import image1 from './public/image1.jpeg';
 import styles from './stylesheets/App.module.css';
 import imageMapResize from 'image-map-resizer';
+import StartModal from './StartModal';
 
 function App() {
 	useEffect(() => {
 		imageMapResize();
 	}, []);
 
+	const [isModalOpen, setIsModalOpen] = useState(true);
+
 	return (
 		<>
+			<StartModal
+				open={isModalOpen}
+				closeModal={() => {
+					setIsModalOpen(false);
+				}}
+			/>
 			<div className={styles.container}>
 				<img
 					src={image1}
---
+++ frontend/src/StartModal.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import style from './stylesheets/StartModal.module.css';
+
+export default function StartModal({ open, closeModal }) {
+	if (!open) {
+		return null;
+	}
+	return (
+		<>
+			<div className={style.overlay} />
+			<div className={style.modal}>
+				<h1>PokéFinder</h1>
+				<p>Welcome to PokéFinder!</p>
+				<p>
+					Complete all the pokemon finding challenges! The clock is ticking!
+				</p>
+				<div className={style.buttonDiv}>
+					<button
+						className={style.modalButton}
+						id="StartGame"
+						onClick={closeModal}
+					>
+						Start
+					</button>
+					<button className={style.modalButton} onClick={closeModal}>
+						View Leaderboards
+					</button>
+				</div>
+			</div>
+		</>
+	);
+}
