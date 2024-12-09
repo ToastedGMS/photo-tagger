8f9e579684be454ca4cc29c0973baf154b410105
ToastedGMS <gabrielsiqueira3052@gmail.com>
2024-12-09 20:15:45 -0300

    add more challenges
    need to fix the multiple click bug!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,11 @@ function App() {
 	}, []);
 
 	const [isModalOpen, setIsModalOpen] = useState(true);
-	const [isChecked, setIsChecked] = useState(false);
+	const [charmanderCheck, setCharmanderCheck] = useState(false);
+	const [spearowCheck, setSpearowCheck] = useState(false);
+	const [teddiursaCheck, setTeddiursaCheck] = useState(false);
+	const [qwilfishCheck, setqwilfishCheck] = useState(0);
+	const [caterpieCheck, setCaterpieCheck] = useState(0);
 
 	return (
 		<>
@@ -22,22 +26,167 @@ function App() {
 				}}
 			/>
 
-			<List open={isModalOpen} charmander={isChecked} />
+			<List
+				open={isModalOpen}
+				checks={{
+					charmanderCheck,
+					spearowCheck,
+					teddiursaCheck,
+					qwilfishCheck,
+					caterpieCheck,
+				}}
+			/>
 			<div className={styles.container}>
 				<img
 					src={image1}
 					alt="Example Image"
 					className={styles.image1}
-					useMap="#charmander"
+					useMap="#pokemon"
 				/>
-				<map name="charmander">
+				<map name="pokemon">
 					<area
-						shape="rect"
-						coords="481,615,545,680"
+						target=""
+						alt="Charmander"
 						title="Charmander"
-						className={styles.pokemon}
-						onClick={() => {
-							setIsChecked(true);
+						href=""
+						coords="490,633,498,653,502,671,519,677,534,669,540,655,551,607,529,622,497,616"
+						shape="poly"
+						onClick={(e) => {
+							e.preventDefault();
+							setCharmanderCheck(true);
+						}}
+					/>
+					<area
+						target=""
+						alt="Spearow"
+						title="Spearow"
+						href=""
+						coords="167,853,183,841,184,815,175,810,153,817,151,834"
+						shape="poly"
+						onClick={(e) => {
+							e.preventDefault();
+							setSpearowCheck(true);
+						}}
+					/>
+					<area
+						target=""
+						alt="Teddiursa"
+						title="Teddiursa"
+						href=""
+						coords="653,578,663,594,660,612,685,615,688,588,685,571"
+						shape="poly"
+						onClick={(e) => {
+							e.preventDefault();
+							setTeddiursaCheck(true);
+						}}
+					/>
+					<area
+						target=""
+						alt="Qwilfish"
+						title="Qwilfish"
+						href=""
+						coords="208,76,28"
+						shape="circle"
+						onClick={(e) => {
+							e.preventDefault();
+							setqwilfishCheck((prevstate) => prevstate + 1);
+							e.target.style.pointerEvents = none;
+						}}
+					/>
+					<area
+						target=""
+						alt="Qwilfish"
+						title="Qwilfish"
+						href=""
+						coords="571,151,27"
+						shape="circle"
+						onClick={(e) => {
+							e.preventDefault();
+							setqwilfishCheck((prevstate) => prevstate + 1);
+						}}
+					/>
+					<area
+						target=""
+						alt="Qwilfish"
+						title="Qwilfish"
+						href=""
+						coords="453,262,29"
+						shape="circle"
+						onClick={(e) => {
+							e.preventDefault();
+							setqwilfishCheck((prevstate) => prevstate + 1);
+						}}
+					/>
+					<area
+						target=""
+						alt="Qwilfish"
+						title="Qwilfish"
+						href=""
+						coords="470,314,29"
+						shape="circle"
+						onClick={(e) => {
+							e.preventDefault();
+							setqwilfishCheck((prevstate) => prevstate + 1);
+						}}
+					/>
+					<area
+						target=""
+						alt="Qwilfish"
+						title="Qwilfish"
+						href=""
+						coords="358,301,29"
+						shape="circle"
+						onClick={(e) => {
+							e.preventDefault();
+							setqwilfishCheck((prevstate) => prevstate + 1);
+						}}
+					/>
+					<area
+						target=""
+						alt="Qwilfish"
+						title="Qwilfish"
+						href=""
+						coords="262,745,26"
+						shape="circle"
+						onClick={(e) => {
+							e.preventDefault();
+							setqwilfishCheck((prevstate) => prevstate + 1);
+						}}
+					/>
+					<area
+						target=""
+						alt="Caterpie"
+						title="Caterpie"
+						href=""
+						coords="17,45,7,79,32,95,61,80,56,43"
+						shape="poly"
+						onClick={(e) => {
+							e.preventDefault();
+							setCaterpieCheck((prevstate) => prevstate + 1);
+						}}
+					/>
+					<area
+						target=""
+						alt="Metapod"
+						title="Metapod"
+						href=""
+						coords="152,508,150,546,174,556,207,553,227,526,192,538,198,519,173,522"
+						shape="poly"
+						onClick={(e) => {
+							e.preventDefault();
+							setCaterpieCheck((prevstate) => prevstate + 1);
+						}}
+					/>
+					<area
+						target=""
+						alt="Butterfree"
+						title="Butterfree"
+						href=""
+						coords="173,412,169,462,160,482,134,489,121,494,90,471,91,427,116,447,143,420"
+						shape="poly"
+						onClick={(e) => {
+							e.preventDefault();
+							setCaterpieCheck((prevstate) => prevstate + 1);
 						}}
 					/>
 				</map>
--- frontend/src/List.jsx
+++ frontend/src/List.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import style from './stylesheets/List.module.css';
 
-export default function List({ open, charmander }) {
+export default function List({ open, checks }) {
+	const {
+		charmanderCheck,
+		spearowCheck,
+		teddiursaCheck,
+		qwilfishCheck,
+		caterpieCheck,
+	} = checks;
+
 	if (open) {
 		return null;
 	}
@@ -10,8 +18,32 @@ export default function List({ open, charmander }) {
 			<div className={style.listDiv}>
 				<ul>
 					<div className={style.liDiv}>
-						<input type="checkbox" checked={charmander} />
+						<input type="checkbox" checked={charmanderCheck} />
 						<li>Find Charmander</li>
+					</div>
+				</ul>
+				<ul>
+					<div className={style.liDiv}>
+						<input type="checkbox" checked={spearowCheck} />
+						<li>Find Spearow</li>
+					</div>
+				</ul>
+				<ul>
+					<div className={style.liDiv}>
+						<input type="checkbox" checked={teddiursaCheck} />
+						<li>Find Teddiursa</li>
+					</div>
+				</ul>
+				<ul>
+					<div className={style.liDiv}>
+						<input type="checkbox" checked={qwilfishCheck === 6} />
+						<li>Find all the Qwilfish ({qwilfishCheck} / 6)</li>
+					</div>
+				</ul>
+				<ul>
+					<div className={style.liDiv}>
+						<input type="checkbox" checked={caterpieCheck === 3} />
+						<li>Find Caterpie and all it's evolutions</li>
 					</div>
 				</ul>
 			</div>
