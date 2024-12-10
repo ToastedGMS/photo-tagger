afd13eea5486c0124aabf9c23dc1a5190307c6f2
ToastedGMS <gabrielsiqueira3052@gmail.com>
2024-12-10 13:30:54 -0300

    allow for multiple device connection on localhost

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
 	plugins: [react()],
+	server: {
+		host: '0.0.0.0', // Allow access from network devices
+	},
 	test: {
 		globals: true,
 		environment: 'jsdom',
