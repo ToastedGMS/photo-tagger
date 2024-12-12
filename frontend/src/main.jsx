7cbd99526b8c64c7ba7b524ac2c4e3ed5a28cd2e
ToastedGMS <gabrielsiqueira3052@gmail.com>
2024-12-12 12:49:18 -0300

    add routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.jsx';
+import Leaderboards from './Leaderboards.jsx';
+import NotFound from './NotFound.jsx';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<App />
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App />} />
+				<Route path="/leaderboards" element={<Leaderboards />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</BrowserRouter>
 	</StrictMode>
 );
